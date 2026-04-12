998f2521c715e4e97df9d0370b563f1fb9afcfd2
Morena Gallardo <morevg.996@gmail.com>
2026-04-12 20:06:07 -0300

    feat: inicializar proyecto con configuración de Maven y estructura básica para en backend.

---
+++ back_procoop/src/main/java/com/procoop/back_procoop/BackProcoopApplication.java
@@ -0,0 +1,13 @@
+package com.procoop.back_procoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackProcoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackProcoopApplication.class, args);
+	}
+
+}
